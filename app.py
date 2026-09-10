2c071add4b1ec72aac937390434fcfc6390d178c
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-09-10 08:14:51 +0700

    update category/update.html

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('layout/base.html')
+
 
 @app.route('/category')
 def index_category():
@@ -11,6 +15,18 @@ def index_category():
 @app.route('/category/create')
 def create_category():
     return render_template('category/create.html')
+
+@app.route('/category/update')
+def update_category():
+    return render_template('category/update.html')
+
+
+# ============Product Route====
+
+@app.route('/product')
+def index_product():
+    return render_template('product/index.html')
+
 
 
 if __name__ == "__main__":
