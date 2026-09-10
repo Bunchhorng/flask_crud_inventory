5e4b6c6db8e22136f3c65760fd83cdb30d4c4a8a
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-09-10 07:56:38 +0700

    update category/create.html

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/category')
 def index_category():
-    return render_template('/category/index.html')
+    return render_template('category/index.html')
 
-if __name__=="__main__":
+
+@app.route('/category/create')
+def create_category():
+    return render_template('category/create.html')
+
+
+if __name__ == "__main__":
     app.run(debug=True)
