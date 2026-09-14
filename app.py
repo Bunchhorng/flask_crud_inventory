769e8579d8d321163d8dcf67d5760fe8f1bc459c
Roeung Bunchhorng <roeungbunchhorng37@gmail.com>
2026-09-14 15:14:48 +0700

    create category

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from connect import connectDB
 
 app = Flask(__name__)
 
-@app.route('/dashboard')
+@app.route('/')
 def dashboard():
     return render_template('layout/base.html')
 
@@ -12,8 +13,21 @@ def index_category():
     return render_template('category/index.html')
 
 
-@app.route('/category/create')
+@app.route('/category/create', methods = ['GET', 'POST'])
 def create_category():
+    if request.method=="POST":
+        name = request.form['name']
+        status = request.form['status']
+
+
+        db = connectDB()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO categories(name, status)" \
+        "VALUES(%s, %s)", (name, status))
+
+        db.commit()
+        cursor = db.close()
+        return redirect(url_for('index_category'))
     return render_template('category/create.html')
 
 @app.route('/category/update')
